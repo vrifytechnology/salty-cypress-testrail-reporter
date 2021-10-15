8947f73df805f0868e4c14f9eb69bd3267d37132
kevVrify <71662874+kevVrify@users.noreply.github.com>
2021-10-14 17:07:50 -0700

    use latest js code from npm package instead of source

--- dist/cypress-testrail-reporter.js
+++ dist/cypress-testrail-reporter.js
@@ -23,7 +23,7 @@ var CypressTestRailReporter = /** @class */ (function (_super) {
         _this.results = [];
         var reporterOptions = options.reporterOptions;
         _this.testRail = new testrail_1.TestRail(reporterOptions);
-        _this.hasBeenCreatedToday = false;
+        _this.isRun = false;
         _this.validate(reporterOptions, 'domain');
         _this.validate(reporterOptions, 'username');
         _this.validate(reporterOptions, 'password');
@@ -33,17 +33,14 @@ var CypressTestRailReporter = /** @class */ (function (_super) {
         runner.on('start', function () {
             console.log("Running Test Case...");
             var executionDateTime = moment().format('L');
-            var name = (reporterOptions.runName || 'Automated Test Run') + " - " + executionDateTime;
+            var name = (reporterOptions.runName || 'Automated test run') + " - " + executionDateTime;
             var description = executionDateTime;
-            if (reporterOptions.createTestRun === true) {
-                _this.testRail.isRunToday().then(function (res) {
-                    _this.hasBeenCreatedToday = res;
-                    console.log(_this.hasBeenCreatedToday);
-                    if (!_this.hasBeenCreatedToday) {
-                        _this.testRail.createRun(name, description);
-                    }
-                });
-            }
+            _this.testRail.isRunToday().then(function (res) {
+                _this.isRun = res;
+                if (!_this.isRun) {
+                    reporterOptions.createTestRun === true && _this.testRail.createRun(name, description);
+                }
+            });
         });
         runner.on('pass', function (test) {
             var _a;
